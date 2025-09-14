42ca89e7d8251fc74dd4f638866af7b603abb399
Uday <23bai70270@cuchd.in>
2025-09-14 18:26:17 +0530

    Added Experiment-9 in React-Project

---
+++ React-Project/Experiment-9/src/App.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  displayInfo() {
+    return `Name: ${this.name}, Age: ${this.age}`;
+  }
+}
+
+class Student extends Person {
+  constructor(name, age, course) {
+    super(name, age);
+    this.course = course;
+  }
+
+  displayInfo() {
+    return `Student -> Name: ${this.name}, Age: ${this.age}, Course: ${this.course}`;
+  }
+}
+
+class Teacher extends Person {
+  constructor(name, age, subject) {
+    super(name, age);
+    this.subject = subject;
+  }
+
+  displayInfo() {
+    return `Teacher -> Name: ${this.name}, Age: ${this.age}, Subject: ${this.subject}`;
+  }
+}
+
+export default function App() {
+  // Create objects
+  const student1 = new Student("Uday ", 20, "Computer Science");
+  const teacher1 = new Teacher("Dr Singh", 40, "CS");
+
+  return (
+    <div style={{ margin: "20px", fontFamily: "Arial" }}>
+      <h2>Experiment 9: Person Class Hierarchy</h2>
+      <p>{student1.displayInfo()}</p>
+      <p>{teacher1.displayInfo()}</p>
+    </div>
+  );
+}
